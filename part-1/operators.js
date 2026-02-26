1d3c2fb864cbd227eac6404220848fee01dae1e3
Aqsa shaikh <shaikhaqsa8620@gmail.com>
2026-02-26 09:51:18 +0530

    add logical & operation file to code base

---
+++ part-1/operators.js
@@ -0,0 +1,25 @@
+let score=102;
+let bonus=25;
+let totalscore=score+bonus;
+
+let addition=4+5;
+let subtract=9-3;
+let mul=3*5;
+let divi=8/2;
+let reminder=9%2;
+let expo=2**3;
+
+let myscore=110;
+myscore++;
+
+let credits=56;
+credits--;
+
+let num1=3;
+let num2=3;
+let num3=6;
+
+console.log(num1==num2);
+console.log(num1!=num3);
+console.log(num1>num3);
+console.log(num1<num3);
